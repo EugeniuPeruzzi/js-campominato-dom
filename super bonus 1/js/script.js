4b48e15f0b53a49853beb797411a7f689a9d08f5
EugeniuPeruzzi <jaka1997@live.it>
2023-05-29 19:17:17 +0200

    added victory sign

--- super bonus 1/js/script.js
+++ super bonus 1/js/script.js
@@ -10,6 +10,7 @@ let punteggio = 0;
 let gameEnded = false;
 //nuova funzione per implementare sia il grid che anche l'array:
 function gridCreator(numeri) {
+    let victory = numeri * numeri;
     //un ciclo che contionua fino a quando no raggiunge i 16 elementi dell'array in modo random.
     while (bombs.length < 16) {
         let randomBomb = Math.floor(Math.random() * 49) + 1;
@@ -48,6 +49,14 @@ function gridCreator(numeri) {
             box.classList.add('clicked');
             box.classList.toggle('box-color');
             punteggio++; 
+          }
+          if (punteggio === victory - bombs){
+            endGame.innerHTML = 
+          `
+          <h2 class="fw-bold display-4">Hai vinto!</h2>
+          <h4 class="fw-bold"> Il tuo punteggio e: ${punteggio}</h4>
+          `
+          gameEnded = true;
           }
         }
       });
@@ -92,18 +101,3 @@ play.addEventListener('click', function(){
   }
 
 });
-
-
-// trovato in rete l'algoritmo di Fisher Yates che mescola gli array in modo randomico.
-// nota importante da segnare, l'algoritmo parte dall'ultimo elemento dell'array er procede fino al primo per questo : (let i = array.length - 1; i > 0; i--)
-// alla variabile i dico e equivalente alla lughezza dell'array - 1 che tradotto vuol dire che deve partire dalla ultima posizione nell'indice quindi numero 16
-// siccome l'algoritmo parte dalla fine dell'array e va all'inizio i non deve essere maggiore di 0. e i-- per decrementare.
-// function shuffleArray(array) {
-    // for (let i = array.length - 1; i > 0; i--) {
-    //     const j = Math.floor(Math.random() * (i));
-    //     [array[i], array[j]] = [array[j], array[i]];        //con questa funzione eseguo lo scambio degli elementi all'interno dell'array        
-    //     return array;
-    // }
-  
-//   let bombsRandomico = shuffleArray(bombs);
-// non saprei porprio come integrarlo nel codice (sadness)
