219fe1fb87b4a6c2dbe4aee394ed6adf65b8a006
EugeniuPeruzzi <jaka1997@live.it>
2023-05-29 16:38:36 +0200

    created while for array lenght

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let grid = document.querySelector('.grid-container');
 let play = document.getElementById('play');
 let boxGrid = document.querySelector('.grid-container');
 let reset  = document.getElementById('reset');
-let bombs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+let bombs = [];
 
 // creo la funzione per generare il grid
 function gridCreator(numeri) {
@@ -22,6 +22,36 @@ function gridCreator(numeri) {
       boxGrid.appendChild(box);
     }
 }
+
+
+//nuova funzione per implementare sia il grid che anche l'array:
+function gridCreator(numeri) {
+    //un ciclo che contionua fino a quando no raggiunge i 16 array in modo random.
+    while (bombs.length < 16) {
+        let randomArray = Math.floor(Math.random() * (numeri * numeri)) + 1;
+        if (!bombs.includes(randomArray)) {
+          bombs.push(randomArray);
+        }
+    }
+
+
+
+    for (let i = 1; i <= numeri*numeri; i++) {
+
+      let box = document.createElement('div');
+      box.classList.add('box');
+      box.style.width = `calc(100% / ${numeri})`;
+      box.innerHTML = `<a href="#">${i}</a>`;
+      
+      box.addEventListener('click', function() {
+        box.classList.toggle('box-color');
+      });
+      
+      boxGrid.appendChild(box);
+    }
+}
+
+
 
 
 // al click di questo tasto i resetta tutta la pagina (problea: se l'utente cambia griglia e clicka rest e come se generasse un campo nuovo)
@@ -66,12 +96,13 @@ play.addEventListener('click', function(){
 // nota importante da segnare, l'algoritmo parte dall'ultimo elemento dell'array er procede fino al primo per questo : (let i = array.length - 1; i > 0; i--)
 // alla variabile i dico e equivalente alla lughezza dell'array - 1 che tradotto vuol dire che deve partire dalla ultima posizione nell'indice quindi numero 16
 // siccome l'algoritmo parte dalla fine dell'array e va all'inizio i non deve essere maggiore di 0. e i-- per decrementare.
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i));
-      [array[i], array[j]] = [array[j], array[i]];        //con questa funzione eseguo lo scambio degli elementi all'interno dell'array
-      }
-    return array;
-  }
+// function shuffleArray(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//       const j = Math.floor(Math.random() * (i));
+//       [array[i], array[j]] = [array[j], array[i]];        //con questa funzione eseguo lo scambio degli elementi all'interno dell'array
+//       }
+//     return array;
+//   }
   
 //   let bombsRandomico = shuffleArray(bombs);
+// non saprei porprio come integrarlo nel codice (sadness)
